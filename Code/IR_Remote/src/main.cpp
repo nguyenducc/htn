2c97ba9c4cb4bf99bb4828633c6ed97fc94e18ff
hoangthemanhzin <toan6111975@gmail.com>
2023-12-07 16:38:40 +0700

    update final code

--- Code/IR_Remote/src/main.cpp
+++ Code/IR_Remote/src/main.cpp
@@ -1,144 +1,180 @@
 #include <Arduino.h>
 #include <IRremote.h>
 
-//#define true 1 = high = den sang
-//#define false 0 = low = den tat
-
-// high = da bam nut bam
-// low = chua bam nut bam
-
-
 // Khai bao bien dieu khien nut bam continuous va single-shot
 // Declare the conditions of continuous and single-shot nut consumption
-const int continuousButtonPin = 6; // setup Continuous
-const int singleButtonPin7 = 7; // setup single-shot
-const int singleButtonPin8 = 8; // setup single-shot
-const int irLedPin = 2;       // setup ir
+const int continuousButtonPin4 = 4; // setup Continuous
+const int continuousButtonPin5 = 5; // setup Continuous
+const int continuousButtonPin6 = 6; // setup Continuous
+const int irLedPin = 2;             // setup ir
 IRsend irsend(irLedPin);
 
-int continuousButtonState = HIGH;   // Trạng thái trước của nút bấm continuous
-int singleButtonState7 = HIGH;   // Trạng thái trước của nút bấm single
-int singleButtonState8 = HIGH;   // Trạng thái trước của nút bấm single
+int continuousButtonState4 = HIGH; // Trạng thái trước của nút bấm Continuous
+int continuousButtonState5 = HIGH; // Trạng thái trước của nút bấm Continuous
+int continuousButtonState6 = HIGH; // Trạng thái trước của nút bấm Continuous
 
-unsigned long continuousTime = 0;   // Thời gian nhấn nút bấm 1
-unsigned long singleTime = 0;   // Thời gian nhấn nút bấm 2
+unsigned long continuousTime = 0; // Thời gian nhấn nút bấm 1
+unsigned long singleTime = 0;     // Thời gian nhấn nút bấm 2
 
-const int carrierFrequency = 38000; // Tần số sóng mang là 38kHz
+const int carrierFrequency = 38000;   // Tần số sóng mang là 38kHz
+const int maxSize = 23;               // Số phần tử tối đa của mảng rawData
+unsigned int rawDataConvert[maxSize]; // Khai báo mảng rawData
 
 // Dãy mang cua command Word cua cac key no : 6,7,8
-bool dataBits7[] = {1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0};
+// key1 : {1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0}
+// key2 : {1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0}
+// key3 : {1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0}
+// key4 : {1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0}
+// key5 : {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0}
 
-bool dataBits8[] = {1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0};
+bool dataBits1[] = {1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0};
+
+bool dataBits2[] = {1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0};
+bool dataBits4[] = {1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0}; //key 4
+
+bool dataBits5[] = {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0}; //key 5
+
+bool dataBits3[] = {1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0}; //key 3
 
 // Hằng số thời gian truyền mỗi bit
-const unsigned long bitDuration =  1000000 / carrierFrequency;
-
-// Hàm gửi một bit
-
-void sendBit(bool bitValue) {
-  if (bitValue) {
-    //digitalWrite(irLedPin, LOW);
-    noTone(irLedPin); // Dừng lại tín hiệu PWM
-    delayMicroseconds(bitDuration * 3);
-    //digitalWrite(irLedPin, HIGH);
-    tone(irLedPin, carrierFrequency); // Gửi tín hiệu PWM ở tần số sóng mang
-    delayMicroseconds(bitDuration);
-  } else {
-    //digitalWrite(irLedPin, LOW);
-    noTone(irLedPin); // Dừng lại tín hiệu PWM
-    delayMicroseconds(bitDuration);
-    //digitalWrite(irLedPin, HIGH);
-    tone(irLedPin, carrierFrequency); // Gửi tín hiệu PWM ở tần số sóng mang
-    delayMicroseconds(bitDuration * 3);
-
-  }
-}
-
-void sendBit(bool bitValue) {
-  if (bitValue) {
-    irsend.mark(bitDuration);
-    irsend.space(bitDuration * 3);
-  } else {
-    irsend.mark(bitDuration);
-    irsend.space(bitDuration);
-    irsend.mark(bitDuration * 3);
-  }
-}
-
-// Hàm gửi SINGLE-SHOT SIGNAL
-void sendSingleShotSignal(bool dataBits[]) {
-  // Gửi từng bit trong dãy
-  for (int i = 0; i < 12; i++) {
-    sendBit(dataBits[i]);
-  }
-}
+const unsigned long bitDuration = 1000000 / carrierFrequency;
 
 // Hàm gửi CONTINUOUS SIGNAL
-void sendContinuousSignal(int signalNumber) {
+void sendContinuousSignal(int signalNumber)
+{
   // TODO: Thực hiện hành động khi gửi CONTINUOUS SIGNAL cho tín hiệu signalNumber
 }
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(continuousButtonPin, INPUT_PULLUP);
-  pinMode(singleButtonPin7, INPUT_PULLUP);
-  pinMode(singleButtonPin8, INPUT_PULLUP);
-  pinMode(irLedPin,OUTPUT);
+void convertData(bool dataBits[])
+{
+  for (int i = 0; i < 12; i++)
+  {
+    bool bitValue = dataBits[i];
+
+    unsigned int *firstValue = &rawDataConvert[2 * i];
+    unsigned int *secondValue = &rawDataConvert[2 * i + 1];
+
+    if (i == 11)
+    {
+      if (bitValue)
+      {
+        *firstValue = 1260;
+      }
+      else
+      {
+        *firstValue = 420;
+      }
+    }
+    else
+    {
+      if (bitValue)
+      {
+        Serial.println("dung");
+        *firstValue = 1260;
+        *secondValue = 420;
+      }
+      else
+      {
+        *firstValue = 420;
+        *secondValue = 1260;
+      }
+    }
+  }
 }
 
-void loop() {
-  int currentContinuous = digitalRead(continuousButtonPin);
-  int currentSingle7 = digitalRead(singleButtonPin7);
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(continuousButtonPin4, INPUT_PULLUP);
+  pinMode(continuousButtonPin5, INPUT_PULLUP);
+  pinMode(continuousButtonPin6, INPUT_PULLUP);
+  pinMode(irLedPin, OUTPUT);
+}
 
-  int currentSingle8 = digitalRead(singleButtonPin8);
+void loop()
+{
+  int currentContinuous4 = digitalRead(continuousButtonPin4);
+  int currentContinuous5 = digitalRead(continuousButtonPin5);
+  int currentContinuous6 = digitalRead(continuousButtonPin6);
 
-  // Ham gui tin hieu conyinuos khi du nut nhan lau :   
-  if(currentContinuous != continuousButtonState){
-      // Check xem thu trang thai cua nut nhan continous da hop ly hay chua : 
-    if(currentContinuous == LOW){
-        continuousTime = millis();
-    }else{
-        unsigned long continuousReleaseTime = millis();
-        unsigned long continuousHoldTime = continuousTime - continuousReleaseTime;
-        if(continuousHoldTime < 200){
-          // Chua co du kien nao de xu ly
-        }else{
-          //  sendContinuousSignal(1);
-        }
+  // Ham gui tin hieu continuos khi du nut nhan lau :
+  if (currentContinuous4 != continuousButtonState4)
+  {
+    convertData(dataBits1);
+    while (1)
+    {
+      // unsigned int rawData[] = {1300, 460, 420, 1360, 400, 1360, 1280, 440, 440, 1340, 400, 1360, 400, 1360, 400, 1340, 420, 1340, 1360, 400, 440, 1280,  440};
+      irsend.sendRaw(rawDataConvert, 24, 38);
+      delay(32);
+      irsend.sendRaw(rawDataConvert, 24, 38);
+      if (digitalRead(continuousButtonPin4) == HIGH)
+      {
+        break;
+      }
+      delayMicroseconds(420 * 208);
+      if (digitalRead(continuousButtonPin4) == HIGH)
+      {
+        irsend.sendRaw(rawDataConvert, 24, 38);
+        delay(32);
+        irsend.sendRaw(rawDataConvert, 24, 38);
+        break;
+      }
     }
   }
 
-  // Ham gui tin hieu single shot khi nhan nut o day voi command Word la 7: 
-  if(currentSingle7 != singleButtonState7){
-    if(currentSingle7 == LOW){
-      Serial.print("dung");
-      //digitalWrite(irLedPin, HIGH);
-      unsigned int rawData[] = {4500, 4500, 560, 1600, 560, 1600, 560, 1600, 560, 550, 560, 550, 560, 550, 560, 550, 560, 550, 560, 550, 560, 550, 560, 550, 560, 550, 560, 550, 560, 1600, 560, 1600, 560, 1600, 560};
-      irsend.sendRaw(rawData, 12, 38);
-      sendSingleShotSignal(dataBits7);
-      sendSingleShotSignal(dataBits7);
-    }else{
-      //digitalWrite(irLedPin, LOW);
-      noTone(irLedPin); // Dừng lại tín hiệu PWM
+  if (currentContinuous5 != continuousButtonState5)
+  {
+    convertData(dataBits1);
+    while (1)
+    {
+      // unsigned int rawData[] = {1300, 460, 420, 1360, 400, 1360, 1280, 440, 440, 1340, 400, 1360, 400, 1360, 400, 1340, 420, 1340, 1360, 400, 440, 1280,  440};
+      irsend.sendRaw(rawDataConvert, 24, 38);
+      delay(32);
+      irsend.sendRaw(rawDataConvert, 24, 38);
+      if (digitalRead(continuousButtonPin5) == HIGH)
+      {
+        break;
+      }
+      delayMicroseconds(420 * 208);
+      if (digitalRead(continuousButtonPin5) == HIGH)
+      {
+        irsend.sendRaw(rawDataConvert, 24, 38);
+        delay(32);
+        irsend.sendRaw(rawDataConvert, 24, 38);
+        break;
+      }
     }
   }
 
-  // Ham gui tin hieu single shot khi nhan nut o day voi command Word la 8: 
-  if(currentSingle8 != singleButtonState8){
-    if(currentSingle8 == LOW){
-      sendSingleShotSignal(dataBits8);
-      sendSingleShotSignal(dataBits8);
-    }else{
-      noTone(irLedPin); // Dừng lại tín hiệu PWM   
+  if (currentContinuous6 != continuousButtonState6)
+  {
+    convertData(dataBits2);
+    while (1)
+    {
+      // unsigned int rawData[] = {1300, 460, 420, 1360, 400, 1360, 1280, 440, 440, 1340, 400, 1360, 400, 1360, 400, 1340, 420, 1340, 1360, 400, 440, 1280,  440};
+      irsend.sendRaw(rawDataConvert, 24, 38);
+      delay(32);
+      irsend.sendRaw(rawDataConvert, 24, 38);
+      if (digitalRead(continuousButtonPin6) == HIGH)
+      {
+        break;
+      }
+      delayMicroseconds(420 * 208);
+      if (digitalRead(continuousButtonPin6) == HIGH) 
+      {
+        irsend.sendRaw(rawDataConvert, 24, 38);
+        delay(32);
+        irsend.sendRaw(rawDataConvert, 24, 38);
+        break;
+      }
     }
   }
-  
+
   /*
-  * Cap nhat trang thai truoc cua dau 6 va dau 7
-  * Lay thong tin tu cac chan 6 va 7 cua vi dieu khien 
-  */ 
-  continuousButtonState = currentContinuous; 
-  singleButtonState7 = currentSingle7;
-  singleButtonState8 = currentSingle8;
-    
-  }
+   * Cap nhat trang thai truoc cua dau 6 va dau 7
+   * Lay thong tin tu cac chan 6 va 7 cua vi dieu khien
+   */
+  continuousButtonState4 = currentContinuous4;
+  continuousButtonState5 = currentContinuous5;
+  continuousButtonState6 = currentContinuous6;
+}
